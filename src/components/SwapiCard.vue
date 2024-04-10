af23fad98526e335faa88de43d2a7b23aee8acf6
Piotr Pachocki <piotr.pachocki@l7informatics.com>
2024-04-10 23:00:00 +0200

    add swapi card details

--- src/components/SwapiCard.vue
+++ src/components/SwapiCard.vue
@@ -6,9 +6,14 @@
         <v-list dense>
           <v-list-item>
             <v-list-item-title>{{ cardDetails.name }}</v-list-item-title>
+            <v-list-item-subtitle>Type: {{ cardDetails.resourceType }}</v-list-item-subtitle>
             <v-list-item-subtitle>Height: {{ cardDetails.height }} cm</v-list-item-subtitle>
             <v-list-item-subtitle>Mass: {{ cardDetails.mass }} kg</v-list-item-subtitle>
             <v-list-item-subtitle>Gender: {{ cardDetails.gender }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Birth Year: {{ cardDetails.birthYear }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Eye Color: {{ cardDetails.eyeColor }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Skin Color: {{ cardDetails.skinColor }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Hair Color: {{ cardDetails.hairColor }}</v-list-item-subtitle>
           </v-list-item>
         </v-list>
       </template>
@@ -16,12 +21,26 @@
         <v-list dense>
           <v-list-item>
             <v-list-item-title>{{ cardDetails.name }}</v-list-item-title>
+            <v-list-item-subtitle>Type: {{ cardDetails.resourceType }}</v-list-item-subtitle>
             <v-list-item-subtitle>Crew: {{ cardDetails.crew }}</v-list-item-subtitle>
             <v-list-item-subtitle>Model: {{ cardDetails.model }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Passengers: {{ cardDetails.passengers }}</v-list-item-subtitle>
             <v-list-item-subtitle
               >Manufacturer: {{ cardDetails.manufacturer }}</v-list-item-subtitle
             >
             <v-list-item-subtitle>Class: {{ cardDetails.starshipClass }}</v-list-item-subtitle>
+            <v-list-item-subtitle>Consumables: {{ cardDetails.consumables }}</v-list-item-subtitle>
+            <v-list-item-subtitle
+              >Max Atmosphering Speed: {{ cardDetails.maxAtmospheringSpeed }}</v-list-item-subtitle
+            >
+            <v-list-item-subtitle
+              >Cost In Creadits: {{ cardDetails.costInCredits }}</v-list-item-subtitle
+            >
+            <v-list-item-subtitle> Length: {{ cardDetails.length }} </v-list-item-subtitle>
+            <v-list-item-subtitle
+              >Cargo Capacity: {{ cardDetails.cargoCapacity }}</v-list-item-subtitle
+            >
+            <v-list-item-subtitle> MGLT: {{ cardDetails.MGLT }} </v-list-item-subtitle>
           </v-list-item>
         </v-list>
       </template>
@@ -51,6 +70,6 @@ export default {
     return {
       SUPPORTED_SWAPI_TYPES
     }
-  },
+  }
 }
 </script>
